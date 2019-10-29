c77317184b33490b29594fda81841d09b0415fe2
benlegendj <benlegendj@gmail.com>
2019-10-29 06:51:46 +0300

    working with arrays in firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,6 +14,40 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
+// database.ref('notes/-LsKWIldzV-MGwGXjLxf').update({
+// 	title: 'books',
+// });
+
+// database.ref('notes').push({
+// 	id: 3,
+// 	title: 'asadsfddf',
+// });
+
+const expenses1 = {
+	id: '1',
+	description: 'Gum',
+	note: '',
+	amount: 195,
+	createdAt: 0,
+};
+const expense2 = {
+	id: '9',
+	description: 'Coffee',
+	note: '',
+	amount: 1000,
+	createdAt: 0,
+};
+database.ref('expenses').push(expenses1);
+database.ref('expenses').push(expense2);
+// };
+// const notes = [
+// 	{
+// 		id: 12,
+// 		title: 'first match',
+// 		body: 'this is the body',
+// 	},
+// ];
+// database.ref().set(notes);
 // ref --> used for grouping data i.e ref for users
 // if nothing is parsed in data is stored in the root directory
 
@@ -47,23 +81,24 @@ const database = firebase.database();
 
 // fetch data keeping track of changes
 // uses callback fn instead of promises
-database.ref('location').on(
-	'value',
-	(snapshot) => {
-		const value = snapshot.val();
-		console.log(value);
-	},
-	(e) => {
-		console.log('something went wrong', e);
-	}
-);
+// database.ref('location').on(
+// 	'value',
+// 	(snapshot) => {
+// 		const value = snapshot.val();
+// 		console.log(value);
+// 	},
+// 	(e) => {
+// 		console.log('something went wrong', e);
+// 	}
+// );
 // to unsubscribe we use ref().off()
 // one can unsubscribe a single item rel().off(subscribe_function)
 // e.g
-const onValueChange = (snapshot) => {
-	const value = snapshot.val();
-	console.log(value);
-};
-database.ref('location').of(onValueChange, (e) => {
-	console.log('something went wrong', e);
-});
+
+// const onValueChange = (snapshot) => {
+// 	const value = snapshot.val();
+// 	console.log(value);
+// };
+// database.ref('location').of(onValueChange, (e) => {
+// 	console.log('something went wrong', e);
+// });
